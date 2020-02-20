13aa2deb7624a70040037d3019b9b0f8950c8a3b
Multarix <darien2105@gmail.com>
2020-02-20 21:09:24 +1000

    Added script.js filey

---
+++ script.js
@@ -0,0 +1,7 @@
+function hideBlocked(){
+    const blocked = document.querySelectorAll('[class^="groupStart"]'); // Find all "Blocked Messages"
+    blocked.forEach(blokMsg => {
+        if(blokMsg.style.display !== "none") blokMsg.style.display = "none"; // Hide the message if it's not already hidden.
+    });
+};
+setInterval(hideBlocked, 500); // Repeat every half second. Recommended to keep at 500, but raise/ lower if you wish.

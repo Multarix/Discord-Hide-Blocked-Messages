0a8f23cbce405cf50df977e701d37612b03ed5ea
Tristan Wiley <tristan@tristanwiley.com>
2022-08-09 20:29:33 -0700

    Update script to apply stylesheet change instead of refreshing every half second

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-function hideBlocked(){
-    const blocked = document.querySelectorAll('[class^="groupStart"]'); // Find all "Blocked Messages"
-    blocked.forEach(blokMsg => {
-        if(blokMsg.style.display !== "none") blokMsg.style.display = "none"; // Hide the message if it's not already hidden.
-    });
-};
-setInterval(hideBlocked, 500); // Repeat every half second. Recommended to keep at 500, but raise/ lower if you wish.
+const sheet = window.document.styleSheets[0];
+sheet.insertRule(`ol > [class^='groupStart-'] { display: none; }`, sheet.cssRules.length);
--- script.user.js
+++ script.user.js
@@ -6,15 +6,10 @@
 // @homepageURL https://github.com/Multarix/Discord-Hide-Blocked-Messages
 // @supportURL  https://github.com/Multarix/Discord-Hide-Blocked-Messages/issues
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      Anonymous
 // @description 10/28/2020, 1:37:40 PM
 // ==/UserScript==
 
-function hideBlocked(){
-    const blocked = document.querySelectorAll('[class^="groupStart"]'); // Find all "Blocked Messages"
-    blocked.forEach(blokMsg => {
-        if(blokMsg.style.display !== "none") blokMsg.style.display = "none"; // Hide the message if it's not already hidden.
-    });
-};
-setInterval(hideBlocked, 500); // Repeat every half second. Recommended to keep at 500, but raise/ lower if you wish.
+const sheet = window.document.styleSheets[0];
+sheet.insertRule(`ol > [class^='groupStart-'] { display: none; }`, sheet.cssRules.length);

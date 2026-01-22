811c50a480886ade95eba71977b59f630e949d10
Darien <darien2105@gmail.com>
2026-01-22 15:42:53 +1000

    Update version and description in script.user.js
    Fixed not working due to css changing as reported in #11

--- script.user.js
+++ script.user.js
@@ -6,11 +6,11 @@
 // @homepageURL https://github.com/Multarix/Discord-Hide-Blocked-Messages
 // @supportURL  https://github.com/Multarix/Discord-Hide-Blocked-Messages/issues
 // @grant       none
-// @version     2.1
+// @version     2.1.1
 // @author      Multarix
-// @description Working as of: 28/10/2022
+// @description Working as of: 22/01/2026
 // ==/UserScript==
 
-const css = `ol > [class^='groupStart_'] { display: none; }`
+const css = `ol > [class*='groupStart'] { display: none; }`
 const style = document.createElement('style'); style.innerHTML = css;
 document.body.appendChild(style);

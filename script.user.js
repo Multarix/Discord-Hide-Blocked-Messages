e22cbaae87e1654bb9aedbf6346fa8df216772d6
Darien <darien2105@gmail.com>
2026-05-06 14:39:20 +1000

    Update version and improve CSS selector for hiding messages

--- script.user.js
+++ script.user.js
@@ -6,11 +6,11 @@
 // @homepageURL https://github.com/Multarix/Discord-Hide-Blocked-Messages
 // @supportURL  https://github.com/Multarix/Discord-Hide-Blocked-Messages/issues
 // @grant       none
-// @version     2.1.1
+// @version     2.2.0
 // @author      Multarix
 // @description Working as of: 22/01/2026
 // ==/UserScript==
 
-const css = `ol > [class*='groupStart'] { display: none; }`
+const css = `div[class*="groupStart"]:has(div[class*="blocked"]) { display: none; }`
 const style = document.createElement('style'); style.innerHTML = css;
 document.body.appendChild(style);
